fb8248f579a29a3920e1762d876e4a99f8f03da7
naoise collins <naoisecollins@gmail.com>
2023-10-05 10:56:15 +0100

    Add Grounded Mechanic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -24,8 +24,20 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         body.velocity = new Vector2(horizontalInput* speed, body.velocity.y);
 
+        if(Input.GetKey(KeyCode.Space)&&grounded)
+        {
+            Jump();
+        }
+
+
     }
     private void Jump(){
-        
+        body.velocity = new Vector2(body.velocity.x,jumpHeight);
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D other){
+        if(other.gameObject.CompareTag("Ground")){
+            grounded = true;
+        }
     }
 }
