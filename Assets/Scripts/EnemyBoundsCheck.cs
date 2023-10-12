eb497edbe581ec11de40763dd9f077648af9dd71
naoise collins <naoisecollins@gmail.com>
2023-10-12 12:57:38 +0100

    Add EnemyBoundingCheck

---
+++ Assets/Scripts/EnemyBoundsCheck.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+
+public class EnemyBoundsCheck : MonoBehaviour
+{
+    [Header("Fall Check")]
+    [SerializeField] private float minY = -10f;  // Y position below which the enemy is considered fallen off
+    
+    [Header("Off Screen Check")]
+    [Tooltip("Extra space before enemy is considered off screen")]
+    [SerializeField] private float screenBuffer = 1.0f;
+
+    private Camera mainCamera;
+    private EnemyController enemyController;
+
+    private void Awake()
+    {
+        mainCamera = Camera.main;
+        enemyController = GetComponent<EnemyController>();
+
+        if(!enemyController)
+        {
+            Debug.LogError("EnemyBoundsCheck is missing EnemyController on the same GameObject!", gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        // Check if enemy has fallen off the main level
+        if (transform.position.y < minY)
+        {
+            enemyController.Die();
+            return;
+        }
+
+        // Check if enemy has moved off the screen
+        Vector3 enemyScreenPos = mainCamera.WorldToViewportPoint(transform.position);
+        if (enemyScreenPos.x < -screenBuffer || enemyScreenPos.x > 1 + screenBuffer ||
+            enemyScreenPos.y < -screenBuffer || enemyScreenPos.y > 1 + screenBuffer)
+        {
+            enemyController.Die();
+        }
+    }
+}
